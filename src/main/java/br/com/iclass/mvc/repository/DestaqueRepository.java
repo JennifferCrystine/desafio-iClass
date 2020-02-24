2f8d55036b1b76bd5589960736be20aad6664215
Jenniffer Crystine <jenniffercrystine@gmail.com>
2020-02-24 16:59:26 -0300

    adiciona dano - inicia destaques - retira sorte

--- src/main/java/br/com/iclass/mvc/repository/DestaqueRepository.java
+++ src/main/java/br/com/iclass/mvc/repository/DestaqueRepository.java
@@ -1,13 +1,13 @@
 package br.com.iclass.mvc.repository;
 
 
+import br.com.iclass.mvc.entity.ItemPersonagem;
+import br.com.iclass.mvc.entity.Personagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-public interface DestaqueRepository extends JpaRepository {
+public interface DestaqueRepository extends JpaRepository<Personagem, Long> {
     @Transactional(readOnly = true)
     @Query("select max(experiencia) from Personagem")
     //@Query("select experiencia from Personagem limit got 3 order by experiencia desc")
